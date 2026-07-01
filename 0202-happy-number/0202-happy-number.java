2070884f829b39ec47a17c70d05102f98527034d
warsinoumaan <mohammadnoumaan34@gmail.com>
2026-07-01 05:53:14 +0530

    Time: 1 ms (78.26%), Space: 42.5 MB (51.07%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+       do {
+            slow = square(slow);
+            fast = square(square(fast));
+        } while (slow != fast);
+
+        return slow == 1;
+    }
+    public int square(int number){
+        int ans = 0;
+     while(number >0){
+    int rem = number%10;
+    ans += rem*rem;
+    number /= 10;
+}
+return ans;
+
+    }
+}
