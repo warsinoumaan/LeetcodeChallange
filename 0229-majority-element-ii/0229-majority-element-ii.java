4f17c29648518010597306e257ab2c5948c61749
warsinoumaan <mohammadnoumaan34@gmail.com>
2026-07-04 08:26:49 +0530

    Time: 2 ms (99.94%), Space: 52.7 MB (40.14%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -0,0 +1,50 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+
+        int count1 = 0, count2 = 0;
+        int cand1 = 0, cand2 = 0;
+
+        // Step 1: Find possible candidates
+        for (int num : nums) {
+
+            if (num == cand1) {
+                count1++;
+            } else if (num == cand2) {
+                count2++;
+            } else if (count1 == 0) {
+                cand1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                cand2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        // Step 2: Verify the candidates
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+            if (num == cand1) {
+                count1++;
+            } else if (num == cand2) {
+                count2++;
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+
+        if (count1 > nums.length / 3) {
+            list.add(cand1);
+        }
+
+        if (count2 > nums.length / 3) {
+            list.add(cand2);
+        }
+
+        return list;
+    }
+}
